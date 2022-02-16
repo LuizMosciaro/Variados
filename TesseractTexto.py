3362bb831a76ee1500c466437c40fe44f71ff983
Luiz Mosciaro <93156186+LuizMosciaro@users.noreply.github.com>
2022-02-16 15:10:40 -0400

    Update TesseractTexto.py

--- TesseractTexto.py
+++ TesseractTexto.py
@@ -1,28 +1,15 @@
 import pytesseract as ps
 import cv2
-#Essa linha faz ter autorização ao aplicativo tesseract.exe
 ps.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\Tesseract.exe"
 
-#lendo a imagem
-imagem = cv2.imread(r'C:\Users\supor\Downloads\texto2.png')
+img = cv2.imread(r"C:\Users\supor\Downloads\texto-notepad.png") #ler
+img1 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #converter cinza
+img2 = cv2.medianBlur(img1,1) #blur/suavizar (0 até 5)
+_,img3 = cv2.threshold(img2,0,255,cv2.THRESH_BINARY +  cv2.THRESH_OTSU)
 
-#convertendo de BLUEGREENRED para RED GREEN BLUE
-imagem_rgb = cv2.cvtColor(imagem,cv2.COLOR_BGR2RGB)
+cv2.imshow("Imagem",img3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-#convertendo de RGB pra Cinza
-imagem_gray = cv2.cvtColor(imagem_rgb,cv2.COLOR_RGB2GRAY)
-
-#cv2.imshow("Thresh",imagem_gray)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-#chamando a leitura
-resultado1 = ps.image_to_string(imagem_rgb)
-print(resultado1)
-
-img_poema = cv2.imread(r'C:\Users\supor\Downloads\poema.png')
-resultado2 = ps.image_to_string(img_poema)
-print(resultado2)
-
-
-
+r = ps.image_to_string(img3)
+print(r)
