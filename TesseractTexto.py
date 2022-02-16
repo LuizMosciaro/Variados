3a24307608e3ec48889d210f44472adbb53ed387
Luiz Mosciaro <93156186+LuizMosciaro@users.noreply.github.com>
2022-02-16 16:47:43 -0400

    Update TesseractTexto.py

--- TesseractTexto.py
+++ TesseractTexto.py
@@ -2,14 +2,20 @@ import pytesseract as ps
 import cv2
 ps.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\Tesseract.exe"
 
-img = cv2.imread(r"C:\Users\supor\Downloads\texto-notepad.png") #ler
+img = cv2.imread(r"C:\Users\supor\Downloads\poema3.jpg") #ler
 img1 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #converter cinza
 img2 = cv2.medianBlur(img1,1) #blur/suavizar (0 até 5)
-_,img3 = cv2.threshold(img2,0,255,cv2.THRESH_BINARY +  cv2.THRESH_OTSU)
+_,img3 = cv2.threshold(img2,1,255,cv2.THRESH_BINARY +  cv2.THRESH_OTSU)
 
-cv2.imshow("Imagem",img3)
+diminuir_largura = 300
+diminuir_altura = 500
+diminuicao = (diminuir_largura,diminuir_altura)
+img_diminuida = cv2.resize(img3,diminuicao,interpolation=cv2.INTER_LINEAR)
+cv2.imshow("Imagem",img_diminuida)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-r = ps.image_to_string(img3)
+my_config = r"--psm 11 --oem 3"
+r = ps.image_to_string(img3,config=my_config)
 print(r)
+
